f6bee69afd0642e47d517f13e065f677caa0484a
Da7y <66154928+Da7y@users.noreply.github.com>
2020-08-20 15:44:20 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+// ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+const UIController = (() => {
+    return {
+        closeNav: () => {
+            $('#navi-toggle').prop('checked', false);
+        }
+    }
+
+})();
+
+const Controller = ((UIController) => {
+    const setupEventListeners = () => {
+        window.addEventListener('hashchange', UIController.closeNav)
+    }
+
+    return {
+        init: () => {
+            setupEventListeners();
+        }
+    }
+
+})(UIController);
+
+Controller.init();
